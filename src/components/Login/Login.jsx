16ba363a04a8c3b41518ec81efc57f208e063f98
Juan Carlos <jcaldama94@gmail.com>
2023-10-01 15:20:22 -0600

    se crea login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { validateEmail } from "./validations";
+import { validatepassword } from "./validations";
+
+export default function Login ({login}) {
+
+    const errorStyle = {
+        color: "rgba(255, 0, 0, 0.8)",
+    
+      };
+
+    const [userData, setUserData] =useState({
+        email:"",
+        password:"",
+    })
+
+    const[errors,setErrors]=useState({  
+        email:"",
+        password:""
+    })
+
+    const handleEmail = (e) =>{
+        const property = e.target.name;
+        const value = e.target.value;
+        setUserData({
+            ...userData,[property]:value
+        })
+        validateEmail({...userData,[property]:value}, setErrors,errors)
+    }
+
+    const handlePass = (e) =>{
+        const property = e.target.name;
+        const value = e.target.value;
+        setUserData({
+            ...userData,[property]:value
+        })
+        validatepassword({...userData,[property]:value}, setErrors,errors)
+    }
+    const handleSubmit = (e)=>{ // manda indormacion 
+        e.preventDefault()
+      
+        login(userData)
+    }
+
+    return(
+        <form  onSubmit={handleSubmit}>
+            <h1 >WELCOME ADMIN</h1>
+
+            <div>
+                <input
+                    type = "text"
+                    placeholder="Email"
+                    name="email"
+                    value={userData.email}
+                    onChange={handleEmail}
+                />
+                <span style={errorStyle}>{errors.email}</span>
+            
+                <input 
+                    type="text"
+                    placeholder="Password"
+                    name="password"
+                    value={userData.password}
+                    onChange={handlePass}
+                />
+                <span style={errorStyle}>{errors.password}</span>
+            
+            
+                {
+                    errors.email || errors.password ?null:<button type="submit">Acceder</button>
+                }
+            
+            </div>
+              
+                
+            
+            
+        </form>
+    )
+}
